76b30edff5ace45ecb40845cdbca6ba6cb138c7c
1zShot <150591076+1zShot@users.noreply.github.com>
2025-11-06 01:10:20 +0300

    Update main.py

--- 1.2lab/main.py
+++ 1.2lab/main.py
@@ -1,2 +1,12 @@
-if __name__ == "__main__":
-    pass
+# Задание №6
+def print_positive_n(A, B, H):
+    if A>=B:
+        print("A должно быть меньше B")
+        return
+    for n in range(A, B, H):
+        if n>0:
+            print(n)
+A=-5
+B=10
+H=2
+print_positive_n(A, B, H)
