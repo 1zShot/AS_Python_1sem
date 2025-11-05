60847b6f95e76c99b18380f0ed611147fc2dd6e2
1zShot <150591076+1zShot@users.noreply.github.com>
2025-11-06 01:25:31 +0300

    Update main.py

--- 1.4lab/main.py
+++ 1.4lab/main.py
@@ -1,2 +1,14 @@
-if __name__ == "__main__":
-    pass
+def product_maxelem(lst):
+    if not lst:
+        return None
+    max_v=max(lst)
+    max_i=lst.i(max_v)
+    rightelem=lst[max_i+1:]
+    prod=1
+    for elem in rightelem:
+        prod*=elem
+    return prod if rightelem else None
+
+lst=[3, 5, 2, 7, 4, 8, 1]
+r=product_maxelem(lst)
+print(r)
