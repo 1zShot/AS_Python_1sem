0516c10b714a9640ce2fc09af81e02ffb3d80b01
1zShot <150591076+1zShot@users.noreply.github.com>
2025-11-06 01:16:29 +0300

    Update main.py

--- 1.3lab/main.py
+++ 1.3lab/main.py
@@ -1,2 +1,12 @@
-if __name__ == "__main__":
-    pass
+import math
+def calculate_sum(n):
+    if n<=0:
+        print("n должно быть натуральным числом")
+        return None
+    ts=0
+    for i in range(1, n+1):
+        total_sum+=((-1)**(i + 1))*math.cos(i)
+    return ts
+n=5
+r=calculate_sum(n)
+print(r)
