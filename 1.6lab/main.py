bf3163d23ab1b3bcca907889ef72543940fd3fd4
1zShot <150591076+1zShot@users.noreply.github.com>
2025-11-06 01:45:45 +0300

    Update main.py

--- 1.6lab/main.py
+++ 1.6lab/main.py
@@ -1,2 +1,10 @@
-if __name__ == "__main__":
-    pass
+def remove_every_nth_character(s, n):
+    if n<=0:
+        return "n>0"
+    r=''.join([char for i, char in enumerate(s) if(i+1)%n!=0])
+    return r
+
+s="abcdefghij"
+n=3
+r=remove_every_nth_character(s, n)
+print(r)
