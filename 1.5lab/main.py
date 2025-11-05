09edc7199c344fc6435d1cd6b8b4bcbf33efa305
1zShot <150591076+1zShot@users.noreply.github.com>
2025-11-06 01:41:22 +0300

    Update main.py

--- 1.5lab/main.py
+++ 1.5lab/main.py
@@ -1,2 +1,8 @@
-if __name__ == "__main__":
-    pass
+def filter_and_sort(lst):
+    fn=[num for num in lst if num>10]
+    sn=sorted(fn)
+    return sn
+
+lst=[5, 12, 3, 18, 7, 21, 10]
+r=filter_and_sort(lst)
+print(r)
