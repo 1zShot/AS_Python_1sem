3e28772f5a6d5dc6567a58d080b631b3d95c29b5
1zShot <150591076+1zShot@users.noreply.github.com>
2025-11-06 01:01:45 +0300

    Update main.py

--- 1.1lab/main.py
+++ 1.1lab/main.py
@@ -1,2 +1,8 @@
-if __name__ == "__main__":
-    pass
+# Задание из банка задач ЕГЭ [https://math-ege.sdamgia.ru/problem?id=77422]
+# Найти наибольшее значение функции y = x^3 - 3x + 4 на отрезке [-2; 0]
+import numpy as np
+# Определяем значения x на интервале [-2, 0]
+x=np.linspace(-2, 0, 1000)
+y=x**3-3*x+4
+max_y=max(y_values)
+print('Максимальное значение:', max_y)
