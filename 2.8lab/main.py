2702aa88baf51ae8f1a6a994258a87eeeff3bceb
1zShot <150591076+1zShot@users.noreply.github.com>
2025-12-22 07:13:42 +0300

    Update main.py

--- 2.8lab/main.py
+++ 2.8lab/main.py
@@ -1,2 +1,9 @@
-if __name__ == "__main__":
-    pass
+a="failik.txt"
+c=0
+with open(a, 'r') as a:
+    z=a.read().split()
+    for t in z:
+        x=int(t)   
+        if x%2==0:
+            c+=1
+print(f"четные числа: {c}")
