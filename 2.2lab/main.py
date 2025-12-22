189bf81d9640b61cdae98550735c7ab8476453a0
1zShot <150591076+1zShot@users.noreply.github.com>
2025-12-22 06:49:46 +0300

    Update main.py

--- 2.2lab/main.py
+++ 2.2lab/main.py
@@ -1,2 +1,16 @@
-if __name__ == "__main__":
-    pass
+import math
+def TriangleP(a, h):
+    a2=a/2
+    bs= math.sqrt(h**2+a2**2)
+    p=a+2*a2
+    return p
+print("=" * 50)
+print("периметр рб")
+print("=" * 50)
+a= float(input("основание: "))
+h= float(input("высота: "))
+if a <= 0 or h <= 0:
+    print("ошибка")
+else:
+    r= TriangleP(a, h)
+    print(f"периметр: {r:.2f}")
