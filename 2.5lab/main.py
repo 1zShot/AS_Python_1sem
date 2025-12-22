a38d75d8c29ef769aa772c4449a467416e812b89
1zShot <150591076+1zShot@users.noreply.github.com>
2025-12-22 07:06:45 +0300

    Update main.py

--- 2.5lab/main.py
+++ 2.5lab/main.py
@@ -1,2 +1,18 @@
-if __name__ == "__main__":
-    pass
+def NOD(a, b):
+    if a==0:
+        return b
+    else:
+        return NOD(b%a,a)
+
+print("четыре натуральных числа:")
+a=int(input("a="))
+b=int(input("b="))
+c=int(input("c="))
+d=int(input("d="))
+
+nod_ab=NOD(a,b)
+print(f"НОД({a},{b})={nod_ab}")
+nod_ac=NOD(a,c)
+print(f"НОД({a},{c})={nod_ac}")
+nod_ad = NOD(a,d)
+print(f"НОД({a},{d})={nod_ad}")
