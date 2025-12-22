a021ff938d6828b3083b04928f43f4cb74edbd25
1zShot <150591076+1zShot@users.noreply.github.com>
2025-12-22 06:56:45 +0300

    Update main.py

--- 2.3lab/main.py
+++ 2.3lab/main.py
@@ -1,2 +1,13 @@
-if __name__ == "__main__":
-    pass
+def SumMtr(m1, m2):
+    r=[]
+    for i in range(len(m1)):
+        s=[]
+        for j in range(len(m1[0])):
+            s.append(m1[i][j] + m2[i][j])
+        r.append(s)
+    return r
+
+a=[[1,2,3],[4,5,6]]
+b=[[10,20,30],[40,50,60]]
+c=SumMtr(a,b)
+print(c)
