e63ff804b073f0d2d2bc668c625127687cac74f2
1zShot <150591076+1zShot@users.noreply.github.com>
2025-12-23 14:08:16 +0300

    Update main.py

--- 2.10lab/main.py
+++ 2.10lab/main.py
@@ -1,2 +1,33 @@
-if __name__ == "__main__":
-    pass
+def power_positive(num, pw):
+    if pw < 0:
+        raise ValueError("Отрицательная степень")
+    return num ** pw
+try:
+    n = float(input("число: "))
+    p = int(input("степень: "))
+    res = power_positive(n, p)
+    print(f"Результат: {res}")
+except ValueError as e:
+    print(f"Ошибка: {e}")
+print()
+def check_even(num):
+    if num % 2 != 0:
+        raise Exception("Число нечет")
+    return True
+try:
+    n = int(input("Введите число для проверки: "))
+    check_even(n)
+    print("Число чет")
+except Exception as e:
+    print(f"Ошибка: {e}")
+print()
+def power_with_assert(num, pw):
+    assert pw >= 0, "Отрицательная степень"
+    return num ** pw
+try:
+    n = float(input("Число: "))
+    p = int(input("Степень: "))
+    res = power_with_assert(n, p)
+    print(f"Результат: {res}")
+except AssertionError as e:
+    print(f"Ошибка: {e}")
